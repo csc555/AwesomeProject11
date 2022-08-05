953875a643c03c8c39f256bb66d26835d2c29368
Blackrose <104713692+BlackroseH@users.noreply.github.com>
2022-08-05 07:08:48 +0200

    He arreglado el manifest.xml y he añadido iconos de fontawesome.

--- Screens/AboutScreen.js
+++ Screens/AboutScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
 const AboutScreen = ({navigation}) => {
   return (
     <View>
       <Text>About</Text>
       <Text>Sobre Nosotros</Text>
+      <Icon name={'rocket'} color={'blue'} size={30} />
     </View>
   );
 };
--- navigation/tab.js
+++ navigation/tab.js
@@ -3,16 +3,40 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import AboutScreen from '../Screens/AboutScreen';
 import HomeScreen from '../Screens/HomeScreen';
 import ListScreen from '../Screens/ListScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
 
-export const BottomTab = ()=> {
-    return(
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="About" component={AboutScreen} />
-            <Tab.Screen name="List" component={ListScreen} />
-        </Tab.Navigator>
-    );
-
-}
+export const BottomTab = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Nosotros"
+        component={AboutScreen}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <Icon name={'heart'} color={'blue'} size={25} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <Icon name={'home'} color={'blue'} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="List"
+        component={ListScreen}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <Icon name={'search'} color={'blue'} size={25} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
