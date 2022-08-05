b89052656d795e2b7cdb3ed0f55bee469e53309c
Blackrose <104713692+BlackroseH@users.noreply.github.com>
2022-08-05 07:41:53 +0200

    He personalizado un poco el Home

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TextInput, SafeAreaView} from 'react-native';
 const HomeScreen = ({navigation}) => {
+  const [text, onChangeText] = React.useState('Useless Text');
+  const [number, onChangeNumber] = React.useState(null);
   return (
-    <View>
-      <Text>Home</Text>
-      <Text>Bienvenido estas en Home</Text>
+    <View
+      name="container"
+      style={{backgroundColor: 'lightgray', height: '100%'}}>
+      <Text style={styles.titleText}>Soy Carmen, Encantada</Text>
+      <Text style={styles.subtitulo}>Login</Text>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          placeholder="Usuario"
+          keyboardType="text"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          placeholder="Tlf"
+          keyboardType="numeric"
+        />
+      </SafeAreaView>
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: '20%',
+    color: 'blue',
+  },
+  subtitulo: {
+    textAlign: 'center',
+    marginTop: '18%',
+    fontSize: 20,
+    color: 'blue',
+  },
+  input: {
+    backgroundColor: 'white',
+    margin: '5%',
+  },
+});
 export default HomeScreen;
